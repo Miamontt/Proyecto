89bb96fcb6c62ab5a1758dd8e6859d45fe90ca7b
Miamontt <miashunaxygarciacontreras@gmail.com>
2025-11-29 19:17:56 -0600

    juego.py
    código de katia

--- juego.py
+++ juego.py
@@ -1,2 +1,40 @@
 #Juego del ahorcado
 #
+import random
+dificultad = input("¿Qué dificultad quiere para el juego? baja/media/alta: ")
+if dificultad == 'baja':
+    lista = ['listi']
+    cadena = "-" * len(lista)
+    intentos = 0
+    while True:
+        def busqueda_lineal (lista, letra):
+            for i in range (len(lista)):
+                if lista [i] == letra:
+                    cadena = cadena [i] + letra + cadena [i + 1]
+                else:
+                    if intentos == 1:
+                        print(" O")
+                    elif intentos == 2:
+                        print(" O")
+                        print("/")
+                    elif intentos == 3:
+                        print(" O")
+                        print("/ |")
+                    elif intentos == 4:
+                        print(" O")
+                        print("/ | \\ ")
+                    elif intentos == 5:
+                        print(" O")
+                        print("/ | \\ ")
+                        print("/ ")
+                    elif intentos == 6:
+                        print(" O")
+                        print("/ | \\ ")
+                        print("/ \\")
+                        print(f"Perdiste el juego, la palabra era, {lista}")
+
+                if cadena == lista:
+                    print(f"¡Felicidades, ganaste el juego! la palabra es, {lista}")
+                    break
+
+letra = input("Ingresa una letra ")
